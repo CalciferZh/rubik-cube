924a14a47ce663882bbd70b18f67cfcf14022498
CalciferZh <CalciferZh@outlook.com>
2017-07-24 16:55:48 +0800

    add favicon

--- script/frontend.js
+++ script/frontend.js
@@ -1,3 +1,13 @@
+function init() {
+  begin();
+  let body = document.getElementsByTagName("body")[0];
+  body.width = 20;
+  body.height = 20;
+  // body.width = window.innerWidth();
+  // body.height = window.innerWidth();
+  // CubieCube.initSolver();
+}
+
 function solveCube() {
   document.getElementById("solution").innerHTML = (new CubieCube(modeling()).solve());
 }
