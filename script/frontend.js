2688c6c065d8a4b55d29e575ffca0bbb7ec932fd
CalciferZh <CalciferZh@outlook.com>
2017-07-25 10:19:49 +0800

    add control info

--- script/frontend.js
+++ script/frontend.js
@@ -245,13 +245,14 @@ function setColor(evt) {
 }
 
 function switchInfo() {
-  let block = document.getElementById("info_content");
-  if (showInfo) {
-    block.setAttribute("class", "invisible");
-    showInfo = false;
-  } else {
-    block.setAttribute("class", "visible");
-    showInfo = true;
+  let blocks = document.getElementsByClassName("info");
+  showInfo = !showInfo;
+  for (let block of blocks) {
+    if (showInfo) {
+      block.setAttribute("class", "info visible");
+    } else {
+      block.setAttribute("class", "info invisible");
+    }
   }
 }
 
